890029d54bfc8dc6534781fc70ecfe2254d15e71
Robby <robby.azucena@gmail.com>
2020-03-20 09:51:34 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 import '../css/main.scss';
 import 'jquery/dist/jquery.min.js';
-import '@popperjs/core/dist/esm/popper.js';
+import '@popperjs/core/dist/esm/popper-lite.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
 
+ 
