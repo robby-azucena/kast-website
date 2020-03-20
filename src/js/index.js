c97d6a24d2a515f6761bf36cd7a1628e3fd1cd53
Robby <robby.azucena@gmail.com>
2020-03-20 17:28:48 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
+import 'bootstrap/scss/bootstrap.scss';
 import '../css/main.scss';
 import '../css/btn.scss';
 import '../css/font.scss';
-import 'jquery/dist/jquery.min.js';
-import 'popper.js/dist/esm/popper.min.js';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/popper.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
@@ -20,3 +21,25 @@ import '../img/send-broadcast.jpg'
 import '../img/contacts.jpg'
 import '../img/dashboard.jpg'
 import '../img/reminder.jpg'
+import '../img/fb-logo.svg'
+import '../img/twitter-logo.svg'
+import '../img/linkedin-logo.png'
+
+
+$(document).scroll(function(){
+    if($(this).scrollTop() > 10 ) {
+        $('.navbar').addClass('scrolled'),
+        $('#kast-logo-nav').attr("src", "img/kast-logo.svg"),
+        $('#button1').addClass('btn-primary'),
+        $('#button2').addClass('btn-primary2')
+        $('#button1').removeClass('btn-nav1'),
+        $('#button2').removeClass('btn-nav2')
+    } else {
+        $('.navbar').removeClass('scrolled'),
+        $('#kast-logo-nav').attr("src", "img/kast-logo-white.svg")
+        $('#button1').removeClass('btn-primary'),
+        $('#button2').removeClass('btn-primary2')
+        $('#button1').addClass('btn-nav1'),
+        $('#button2').addClass('btn-nav2')
+    }
+})
