8b2ada875e3683ecef635a7d882ea70cb3383a2a
Robby <robby.azucena@gmail.com>
2020-03-20 11:42:51 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,13 @@ import 'popper.js/dist/esm/popper.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-
- 
+import '../img/Banner-01.jpg'
+import '../img/globe-logo-110x41_v7.png'
+import '../img/go-bear-logo.png'
+import '../img/golden-haven-logo.png'
+import '../img/metro-logo.svg'
+import '../img/powered-by-echo.png'
+import '../img/primer-logo.png'
+import '../img/UNICEF_Logo.svg'
+import '../img/kast-logo.svg'
+import '../img/kast-logo-white.svg'
