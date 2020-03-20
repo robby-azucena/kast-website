782ed5e91030d87bd8baab4c986e7ed3e78b2a81
Robby <robby.azucena@gmail.com>
2020-03-20 10:44:15 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import '../css/main.scss';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/esm/popper.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 
  
