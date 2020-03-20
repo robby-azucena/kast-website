b826f57913ef5fb99367ecfa0addee1a46b02fe4
Robby <robby.azucena@gmail.com>
2020-03-20 09:47:22 +0800

    setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../css/main.scss';
-import 'jquery/dist/jquery.js';
-import '@popperjs/core/dist/esm/popper-lite.js';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery/dist/jquery.min.js';
+import '@popperjs/core/dist/esm/popper.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 
