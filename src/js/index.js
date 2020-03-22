1b741a919c63871273a48cab7a1bbbd292f5d0ff
Robby <robby.azucena@gmail.com>
2020-03-22 17:11:45 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -26,47 +26,77 @@ import '../img/twitter-logo.svg'
 import '../img/linkedin-logo.png'
 
 
-$(document).scroll(function(){
-    if($(this).scrollTop() > 10 ) {
-        $('.navbar').addClass('scrolled'),
-        $('#kast-logo-nav').attr("src", "img/kast-logo.svg"),
-        $('#button1').addClass('btn-primary'),
-        $('#button2').addClass('btn-primary2')
+$(document).scroll(function () {
+    if ($(this).scrollTop() > 10) {
+        $('.navbar').addClass('scrolled')
+        $('#kast-logo-nav').attr("src", "img/kast-logo.svg")
+        $('#button1').addClass('btn-primary')
         $('#button1').removeClass('btn-nav1')
-        $('.bar1').css({'background-color':'black'})
-        $('.bar2').css({'background-color':'black'})
-        $('.bar3').css({'background-color':'black'})
-    } else {
-        $('.navbar').removeClass('scrolled'),
+        $('.bar1').css({ 'background-color': '#0A85AB' })
+        $('.bar2').css({ 'background-color': '#0A85AB' })
+        $('.bar3').css({ 'background-color': '#0A85AB' })
+    } else if (!$('.navbar-toggler').hasClass('change') || $('.navbar-toggler').hasClass('change')  && $(window).width() > 993) {
+        $('.navbar').removeClass('scrolled')
         $('#kast-logo-nav').attr("src", "img/kast-logo-white.svg")
-        $('#button1').removeClass('btn-primary'),
-        $('#button2').removeClass('btn-primary2')
+        $('#button1').removeClass('btn-primary')
         $('#button1').addClass('btn-nav1')
-        $('.bar1').css({'background-color':'white'})
-        $('.bar2').css({'background-color':'white'})
-        $('.bar3').css({'background-color':'white'})
+        $('.bar1').css({ 'background-color': 'white' })
+        $('.bar2').css({ 'background-color': 'white' })
+        $('.bar3').css({ 'background-color': 'white' })
     }
 })
 
-$(document).ready(function(){
-    
-    $('.navbar-toggler').click(function(e) {
+
+$(window).resize(function () {
+    if ($(window).width() > 993 && $('.navbar-toggler').hasClass('change') && $(document).scrollTop() < 10) {
+        $('.navbar').removeClass('scrolled')
+        $('#kast-logo-nav').attr("src", "img/kast-logo-white.svg")
+        $('#button1').removeClass('btn-primary')
+        $('#button1').addClass('btn-nav1')
+    } else if ($(window).width() < 992.98 && $('.navbar-toggler').hasClass('change') && $(document).scrollTop() < 10) {
+        $('.navbar').addClass('scrolled')
+        $('#kast-logo-nav').attr("src", "img/kast-logo.svg")
+        $('#button1').addClass('btn-primary')
+        $('#button1').removeClass('btn-nav1')
+        $('.bar1').css({ 'background-color': '#0A85AB' })
+            $('.bar2').css({ 'background-color': '#0A85AB' })
+            $('.bar3').css({ 'background-color': '#0A85AB' })
+    } else if ($(window).width() > 993 && $('.navbar-toggler').hasClass('change') && $(document).scrollTop() > 10) {
+
+    } else if ($(window).width() < 992.98 && $('.navbar-toggler').hasClass('change') && $(document).scrollTop() > 10) {
+
+    }
+})
+
+
+
+$(document).ready(function () {
+    $('.navbar-toggler').click(function (e) {
         $(this).toggleClass('change')
-
-        if(!$('.navbar').hasClass('scrolled')) {
+        if (!$('.navbar').hasClass('scrolled') && $('.navbar-toggler').hasClass('change')) {
+            $(".navbar-toggler").css("pointer-events", "none");
             $('.navbar').addClass('scrolled')
-        } else if ($('.navbar').hasClass('scrolled') && $(document).scrollTop() < 10){
+            $('#kast-logo-nav').attr("src", "img/kast-logo.svg")
+            $('#button1').addClass('btn-primary')
+            $('#button1').removeClass('btn-nav1')
+            $('.bar1').css({ 'background-color': '#0A85AB' })
+            $('.bar2').css({ 'background-color': '#0A85AB' })
+            $('.bar3').css({ 'background-color': '#0A85AB' })
+            setTimeout(() => {
+                $(".navbar-toggler").css("pointer-events", "auto");
+            }, 500);
+        } else if ($('.navbar').hasClass('scrolled') && $(document).scrollTop() < 10 && !$('.navbar-toggler').hasClass('change')) {
+            $(".navbar-toggler").css("pointer-events", "none");
             $('.navbar').removeClass('scrolled')
+            $('#kast-logo-nav').attr("src", "img/kast-logo-white.svg")
+            $('#button1').removeClass('btn-primary')
+            $('#button1').addClass('btn-nav1')
+            $('.bar1').css({ 'background-color': 'white' })
+            $('.bar2').css({ 'background-color': 'white' })
+            $('.bar3').css({ 'background-color': 'white' })
+            setTimeout(() => {
+                $(".navbar-toggler").css("pointer-events", "auto");
+            }, 500);
         }
-        
-
-        //$('#button1').toggleClass('btn-nav1 btn-primary')
-        //$('#button2').toggleClass('btn-nav2 btn-primary2')
-        //$('.plugin-Navigation').toggleClass('hide-nav');
-       // $('.plugin-Navigation-list').toggleClass('hide-nav');
-       // $('.plugin-Navigation-item').toggleClass('hide-nav');
     })
-
-
-
 })
