25f3965c8c4163f8be020caf1e38518506d54adb
Robby <robby.azucena@gmail.com>
2020-04-29 13:43:39 +0800

    update responsive

--- src/js/index.js
+++ src/js/index.js
@@ -13,10 +13,10 @@ import '../img/kast-logo-white.svg'
 import '../img/fb-logo.svg'
 import '../img/twitter-logo.svg'
 import '../img/linkedin-logo.png'
-import '../img/create-message.svg'
 import '../img/div-green.svg'
 import '../img/diy.png'
 import '../img/envelope.png'
+import '../img/create-message.png'
 
 
 $(document).scroll(function () {
