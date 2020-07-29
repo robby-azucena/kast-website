77bee4cff592fe1cee0519d2f6448df003c4c58c
Robby <robby.azucena@gmail.com>
2020-07-29 13:33:13 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -24,27 +24,27 @@ module.exports = {
         'babel-polyfill': ['@babel/polyfill'],
         index: './src/js/index.js'
     },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-            chunks: 'all',
-            maxInitialRequests: Infinity,
-            minSize: 0,
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name(module) {
-                        // get the name. E.g. node_modules/packageName/not/this/part.js
-                        // or node_modules/packageName
-                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-                        // npm package names are URL-safe, but some servers don't like @ symbols
-                        return `npm.${packageName.replace('@', '')}`;
-                    },
-                },
-            },
-        },
-    },
+    //optimization: {
+    //    runtimeChunk: 'single',
+    //    splitChunks: {
+    //        chunks: 'all',
+    //        maxInitialRequests: Infinity,
+    //        minSize: 0,
+    //        cacheGroups: {
+    //            vendor: {
+    //                test: /[\\/]node_modules[\\/]/,
+    //                name(module) {
+    //                    // get the name. E.g. node_modules/packageName/not/this/part.js
+    //                    // or node_modules/packageName
+    //                    const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+//
+    //                    // npm package names are URL-safe, but some servers don't like @ symbols
+    //                    return `npm.${packageName.replace('@', '')}`;
+    //                },
+    //            },
+    //        },
+    //    },
+    //},
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'docs')
